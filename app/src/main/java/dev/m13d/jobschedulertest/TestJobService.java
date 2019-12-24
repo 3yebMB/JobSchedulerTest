b0ec1cd19d466990174a8e773bfe3a32cd88b317
Michael <3yebmb@gmail.com>
2019-12-24 14:57:11 +0500

    add methods to start and stop JobService

--- app/src/main/java/dev/m13d/jobschedulertest/TestJobService.java
+++ app/src/main/java/dev/m13d/jobschedulertest/TestJobService.java
@@ -11,11 +11,34 @@ public class TestJobService extends JobService {
 
     @Override
     public boolean onStartJob(JobParameters params) {
-        return false;
+        Log.d(TAG, "Job's started");
+        doBackgroundWork(params);
+        return true;
+    }
+
+    private void doBackgroundWork(final JobParameters params) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    Log.d(TAG, "run: " + i);
+                    if (jobCancelled) { return; }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Log.d(TAG, "Job's finished");
+                jobFinished(params, false);
+            }
+        }).start();
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
-        return false;
+        Log.d(TAG, "Job's cancelled before completion");
+        jobCancelled = true;
+        return true;
     }
 }
