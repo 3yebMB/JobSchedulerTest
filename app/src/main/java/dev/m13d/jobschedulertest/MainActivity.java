3a5cee0ea01d68bace694057d4d9e2bb311c66f3
Michael <3yebmb@gmail.com>
2019-12-24 15:12:03 +0500

    start/stop JobScheduler

--- app/src/main/java/dev/m13d/jobschedulertest/MainActivity.java
+++ app/src/main/java/dev/m13d/jobschedulertest/MainActivity.java
@@ -2,10 +2,16 @@ package dev.m13d.jobschedulertest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +20,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startJob(View view) {
+        ComponentName componentName = new ComponentName(this, TestJobService.class);
+        JobInfo info = new JobInfo.Builder(123, componentName)
+                .setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true)
+                .setPeriodic(15 * 60 * 1000)
+                .build();
+
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        int resultCode = scheduler.schedule(info);
+        if (resultCode == JobScheduler.RESULT_SUCCESS) {
+            Log.d(TAG, "Job's scheduled");
+        } else {
+            Log.d(TAG, "Job's scheduling failed");
+        }
     }
 
     public void stopJob(View view) {
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        scheduler.cancel(123);
+        Log.d(TAG, "Job's cancelled");
     }
 }
